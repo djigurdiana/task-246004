de8e31b2b0ba5a58fa0a73148d0b2a5499708cd6
Diana Nikulina <djigurdiana@mail.ru>
2025-11-19 19:47:14 +0500

    task0.2 done

---
+++ App10.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task10_1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            int n;
+            if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            {
+                Console.WriteLine("Ошибка ввода");
+                return;
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите число {i}:");
+                double ai;
+                if (!double.TryParse(Console.ReadLine(), out ai))
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    return;
+
+                }
+
+                if ((i - 1) % 4 < 2)
+                    sum += ai;
+                else
+                    sum -= ai;
+
+            }
+            Console.WriteLine($"Результат: {sum}");
+
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using System.Runtime.ExceptionServices;
+
+{
+     internal class program
+{
+    static void Main(string[] args)
+    {
+       int [] a = new int [10];
+       var rnd = new Random();
+       for (int i = 0; i < a.Length; i++)
+       {
+           a[i] = rnd.Next(1, 100);
+           
+       }
+       PrintArray(a);
+       Array.Sort(a);
+         PrintArray(a);
+         Array.Sort(a, 0, a.length);
+         
+    }
+}
+static void 
+
+
+
+}
