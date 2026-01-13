e45c09be924315afef3918fb5e412fccc4f91e29
Diana Nikulina <djigurdiana@mail.ru>
2026-01-13 16:02:44 +0500

    uu

---
+++ if-else/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("aaa");
+        int kk = int.Parse(Console.ReadLine());
+        for (kk = ; kk < 10; kk++)
+        {
+            Console.WriteLine(kk);
+        }
+    }
+}
